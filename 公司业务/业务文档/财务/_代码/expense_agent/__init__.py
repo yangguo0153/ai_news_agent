141dc943ddf166182a357b48f9068e7ae1467c47
yangguo0153 <129958726+yangguo0153@users.noreply.github.com>
2026-03-09 14:50:27 +0800

    Delete 公司业务 directory

--- 公司业务/业务文档/财务/_代码/expense_agent/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-expense_agent 包初始化
-"""
-
-from .parser import AttendanceParser, AttendanceRecord
-from .config import *
-
-__version__ = "1.0.0"
-__all__ = ["AttendanceParser", "AttendanceRecord"]
